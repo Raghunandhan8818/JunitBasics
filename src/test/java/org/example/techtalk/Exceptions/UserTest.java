fe34551a2f4bc63c8ae5f1e3973e0efdea071c1e
Raghunandhan AJ <raghunandhan.19cs@kct.ac.in>
2024-06-06 22:13:44 +0530

    Add. JUnit Excetion Testing

---
+++ src/test/java/org/example/techtalk/Exceptions/UserTest.java
@@ -0,0 +1,49 @@
+package org.example.techtalk.Exceptions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenNameIsNull() {
+        User user = new User();
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setName(null);
+        });
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenLengthOfNameIsLessThanThree() {
+
+//        We can obtain the returned exception to test further, e.g. assert the exception message:
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class, () -> {
+                    User user = new User();
+                    user.setName("ab");
+                }
+        );
+
+        assertEquals("Username is too short", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenLengthOfNameIsMoreThan30() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class, () -> {
+                    User user = new User();
+                    user.setName("Chennai Mumbai Bangalore KolKata Pune Hyderabad");
+                }
+        );
+        assertEquals("Username is too long", exception.getMessage());
+    }
+
+}
